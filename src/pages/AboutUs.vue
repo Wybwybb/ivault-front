2c2865b12f833b85c4d3c5b0bc4f7e53434ff379
rmac3392 <ryanjamesmacawili@gmail.com>
2024-03-21 11:24:14 +0800

    test

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -1,2 +1,10 @@
-<template>About us</template>
-<script setup></script>
+<template>
+  <div class="flex justify-center items-center p-5 ">
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
+<script setup>
+import Card from "../components/Card.vue";
+</script>
--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,4 +1,4 @@
 <template>
-Sign up
+
 </template>
 <script setup></script>
